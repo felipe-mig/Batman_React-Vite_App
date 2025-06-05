98af1dc8086415199b1260564f0adc5a08d5f9dc
Felipe Iglesias Garcia <felipemiglesias@gmail.com>
2025-06-06 01:52:23 +1000

    Delete src directory

--- src/components/HomeButton.jsx
+++
@@ -1,8 +0,0 @@
-import logoImg from '../images/logo.svg'
-
-const HomeButton = ({onClick}) => {
-    return(
-        <img className="logo" src={logoImg} onClick={onClick} />
-    );
-};
-export default HomeButton;
